29cd9389189100c6296103d656614db747d0c86e
DanielLin <daniellin0516@gmail.com>
2022-11-23 21:01:01 +0800

    feat: runtime && example

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,11 @@
+export function createComponentInstance(vnode) {
+  const component = {
+    vnode,
+  }
+  return component;
+}
+
+export function setupComponent(instance) {
+  // initProps
+  // initSlots
+}
---
+++ src/runtime-core/render.ts
@@ -0,0 +1,21 @@
+import { createComponentInstance } from "./component";
+
+export function render(vnode, container) {
+  // patch
+  patch(vnode, container);
+}
+function patch(vnode, container) {
+  // 处理组件
+  processComponent(vnode, container);
+
+}
+
+function processComponent(vnode: any, container: any) {
+  mountComponent(vnode);
+}
+
+function mountComponent(vnode: any) {
+  const instance = createComponentInstance(vnode);
+  setupComponent(instance);
+}
+
