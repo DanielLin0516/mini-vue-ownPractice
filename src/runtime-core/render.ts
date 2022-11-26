03c1ad65dba92b4b2fe79416a13e762f1867396e
DanielLin <daniellin0516@gmail.com>
2022-11-26 18:03:54 +0800

    feat: component render

--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared/index";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container) {
@@ -7,8 +8,36 @@ export function render(vnode, container) {
 function patch(vnode, container) {
   // processElement();
   // 处理组件
-  processComponent(vnode, container);
+  if (typeof vnode.type === 'string') {
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container);
+  }
+}
 
+function processElement(vnode: any, container: any) {
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode, container) {
+  const el = document.createElement(vnode.type);
+  const { children, props } = vnode;
+  if (typeof children === "string") {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, el);
+  }
+  for (const key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+  container.append(el);
+}
+
+function mountChildren(vnode: any, container: any) {
+  vnode.children.forEach(vnode => {
+    patch(vnode, container);
+  });
 }
 
 function processComponent(vnode: any, container: any) {
@@ -25,4 +54,10 @@ function setupRenderEffect(instance: any, container) {
   const subTree = instance.render();
   patch(subTree, container);
 }
+
+
+
+
+
+
 
